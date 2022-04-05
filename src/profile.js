4b8135d582a4aafd202dd14e97215c0b32c83175
Jenny Yang <yangxiaojing092@gmail.com>
2022-04-04 22:45:55 -0400

    update about me

--- src/profile.js
+++ src/profile.js
@@ -9,7 +9,7 @@ const animation = {
 };
 // Change your display name on tha landing display
 const header = {
-  name: "a Human",
+  name: "Jenny Yang",
 };
 const background = {
   // Options: Snow or Particle
@@ -20,7 +20,7 @@ const background = {
 const section2title = "About Me";
 const about = {
   paragraph:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ",
+    "I have over 5 years’ experience in the IT industry, mainly focused on front-end development. My tech stack includes HTML, CSS, JavaScript, React, MongoDB, Node.js, TypeScript and Java.",
 };
 // Edit your skill and the percentage you know about it
 // To Add a skill, copy any one below and paste it after the last comma
